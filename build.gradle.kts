7e7677b278993a7f97c1ab5adda5f78e047137af
CameraStellar Dev <dev@camerastellar.local>
2026-03-21 17:38:29 -0500

    [Hito 1.1] Limpieza: Eliminado el código antiguo de React Native tras la migración a Nativo Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,6 @@
-// Archivo de Gradle a nivel de proyecto
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
-}
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+// Archivo de Gradle de Raíz (CameraStellar v3)
+plugins {
+    id("com.android.application") version "8.13.2" apply false
+    id("com.android.library") version "8.13.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,13 @@
+// settings.gradle.kts (CameraStellar v3)
 pluginManagement {
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
     }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
